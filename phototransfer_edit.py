81fc406072bafbbe9a51e50dfdb1e451b0aed144
Colin Lo <locolin99@gmail.com>
2022-05-05 13:44:45 +0800

    Second commit

---
+++ phototransfer_edit.py
@@ -0,0 +1,56 @@
+import os
+import subprocess
+from datetime import date
+
+today = date.today()
+
+today_month = today.strftime("%m %B")
+today_year = today.strftime("%Y")
+
+path = "/Volumes/LHTC Hard Drive 2/Photos/"
+path = os.path.join(path, today_year, today_month)
+
+subprocess.Popen(["open", path])
+
+editing_folder = input("Drag the file here: ")
+editing_folder = editing_folder.replace("\\", "")
+editing_folder = editing_folder + "\b"
+
+# editing_folder = "/Volumes/LHTC Hard Drive 2/Photos/2021/08 August/27_8_2020 Clement Photoshoot"
+
+path, export_folder = os.path.split(editing_folder)
+
+path, month_folder = os.path.split(path)
+
+path, year_folder = os.path.split(path)
+
+export_path = "/Volumes/LHTC Hard Drive 2/Edited Photos/"
+print("\nExport Path: ", os.path.join(export_path, year_folder, month_folder), "\n")
+
+
+def check_folder(cur_path, cur_dir):
+    path_created = os.path.join(cur_path, cur_dir)
+    if os.path.isdir(path_created):
+        print("'" + cur_dir + "'" + " exist\n")
+    else:
+        print("'" + cur_dir + "'" + " does not exist")
+        os.chdir(export_path)
+        try:
+            os.mkdir(cur_dir)
+        except OSError:
+            print("Fail to create folder " + "'" + cur_dir + "'\n")
+        else:
+            print("Successfully created folder " + "'" + cur_dir + "'\n")
+
+
+check_folder(export_path, year_folder)
+export_path = os.path.join(export_path, year_folder)
+
+check_folder(export_path, month_folder)
+export_path = os.path.join(export_path, month_folder)
+
+check_folder(export_path, export_folder)
+
+print("Final Path: " + os.path.join(export_path, export_folder))
+
+subprocess.Popen(["open", export_path])
---
+++ phototransfer_film.py
@@ -0,0 +1,56 @@
+import os
+import subprocess
+from datetime import date
+
+og_path = "/Volumes/LHTC Hard Drive 2/Film/"
+
+today = date.today()
+pathyear = today.strftime("%Y")
+pathmonth = today.strftime("%m %B")
+
+path_test_year = og_path + pathyear
+path_test_month = og_path + pathyear + "/" + pathmonth
+
+print("")
+
+def create_folders(path_test, path_create, cur_date):
+    if os.path.isdir(path_test):
+        print("Folder " + "'" + cur_date + "'" + " exits")
+    else:
+        print("Folder " + "'" + cur_date + "'" + " does not exit")
+        os.chdir(path_create)
+        try:
+            os.mkdir(cur_date)
+        except OSError:
+            print("Fail to create folder " + "'" + cur_date + "'\n")
+        else:
+            print("Successfully created folder " + "'" + cur_date + "'\n")
+
+
+create_folders(path_test_year, og_path, pathyear)
+create_folders(path_test_month, path_test_year, pathmonth)
+
+TOpath = path_test_month
+os.chdir(TOpath)
+print("\nPath:", TOpath)
+
+event = input("\nWhat is this film (Format: brand_filmNameWithISO): ")
+todayfile = today.strftime("%Y-%m-%d")
+dirname = todayfile + " " + event
+print("Directory:", dirname)
+
+print("Status:", end=" ")
+try:
+    os.mkdir(todayfile + " " + event)
+except OSError:
+    print("Fail to create folder " + "'" + dirname + "'")
+else:
+    print("Successfully created folder " + "'" + dirname + "'")
+
+def open_file(path):
+    subprocess.Popen(["open", path])
+
+TO = TOpath + "/" + dirname
+
+open_file("/Users/colinlo/Downloads")
+open_file(TO)
